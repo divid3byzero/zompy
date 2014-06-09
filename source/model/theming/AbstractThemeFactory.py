c1473f0cff9177e7b3fd97dca7632bbf7a875fcb
Benedikt Buchner <benedikt.buchner@masterpayment.com>
2014-06-09 16:59:02 +0200

    - Removed import

--- source/model/theming/AbstractThemeFactory.py
+++ source/model/theming/AbstractThemeFactory.py
@@ -1,9 +1,6 @@
 __author__ = 'bene'
 
 from abc import ABCMeta, abstractmethod
-from twisted.protocols.amp import __metaclass__
-
-
 class AbstractThemeFactory(object):
     __metaclass__ = ABCMeta
 
