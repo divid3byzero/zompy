0d4a0fa4a8cbb501c3ccce933c57ffc3de458076
Benedikt Buchner <benedikt.buchner@masterpayment.com>
2014-06-09 10:58:30 +0200

    - Started implementing dynamic sprite generation needed for themeing

---
+++ source/model/world/ZombieMapFactory.py
@@ -0,0 +1,17 @@
+__author__ = 'bene'
+
+
+from source.model.world.AbstractMapFactory import AbstractMapFactory
+class ZombieMapFactory(AbstractMapFactory):
+
+    def createEnemy(self):
+        pass
+
+    def createPlayer(self):
+        pass
+
+    def createTile(self, elementType):
+        pass
+
+    def __init__(self):
+        pass
