eac77064233c6b526dfafc4554f8ba7215da1501
arabaer <sebastianhanna@hotmail.de>
2014-06-14 23:00:30 +0200

    bugfix

--- source/model/worker/CollisonDetector.py
+++ source/model/worker/CollisonDetector.py
@@ -2,27 +2,24 @@ import pygame
 
 class CollisionDetector(object):
 
-    def __init__(self, playerGroup, enemyGroup, bulletGroup):
-        self.playerGroup = playerGroup
-        self.enemyGroup = enemyGroup
-        self.bulletGroup = bulletGroup
+    def __init__(self):
+        pass
 
+    def checkCollisions(self, playerGroup, enemyGroup, bulletGroup):
+        self.__checkPlayerZombieCollision(playerGroup, enemyGroup)
+        self.__checkZombieBulletCollision(bulletGroup, enemyGroup)
 
-    def checkCollisions(self):
-        self.__checkPlayerZombieCollision()
-        self.__checkZombieBulletCollision()
-
-    def __checkPlayerZombieCollision(self):
+    def __checkPlayerZombieCollision(self, playerGroup, enemyGroup):
         collisions = pygame.sprite.groupcollide(playerGroup, enemyGroup, False, False)
         for k, v in collisions.iteritems():
             for _ in v:
                 k.life -= 1
 
-    def __checkZombieBulletCollision(self):
+    def __checkZombieBulletCollision(self, bulletGroup, enemyGroup):
         collisions = pygame.sprite.groupcollide(bulletGroup, enemyGroup, True, False)
         for k, v in collisions.iteritems():
             for i in v:
                 i.hit()
 
-    def __checkBulletWallCollision(self):
+    def __checkBulletWallCollision(self, wallGroup, bulletGroup):
         collisions = pygame.sprite.groupcollide(wallGroup, bulletGroup, False, True)
