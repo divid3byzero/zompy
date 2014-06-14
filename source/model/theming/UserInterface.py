2308d3c14816d60d485010c875cd39dafad80f4f
Benedikt Buchner <benedikt.buchner@masterpayment.com>
2014-06-15 00:26:31 +0200

    - Implemnted display of player score

--- source/model/theming/UserInterface.py
+++ source/model/theming/UserInterface.py
@@ -1,5 +1,6 @@
 __author__ = 'bene'
 import pygame
+import os
 
 
 class UserInterface(object):
@@ -10,4 +11,8 @@ class UserInterface(object):
 
     def draw(self):
         lifeBar = pygame.Rect(self.screen.get_rect().width - 120, 10, self.player.life, 15)
-        pygame.draw.rect(self.screen, (0,250,0), lifeBar)
+        pygame.draw.rect(self.screen, (106, 227, 36), lifeBar)
+
+        menuFont = pygame.font.Font(os.path.join("resources", "fonts", "PrintChar21.ttf"), 14)
+        menuText = menuFont.render(str(self.player.score), 1, (106, 227, 36))
+        self.screen.blit(menuText, (self.screen.get_rect().x + 10, 10))
